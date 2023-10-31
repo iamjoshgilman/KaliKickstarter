9c6fe8a3706cf6c1e34251d049a18cc13895513f
Joshua Gilman <96031165+WhiteRqbbitt@users.noreply.github.com>
2023-10-31 13:25:10 -0400

    Update kalikickstarter.sh

--- kalikickstarter.sh
+++ kalikickstarter.sh
@@ -27,9 +27,10 @@ function install_package() {
     apt install -y "$1"
     if [[ $? -eq 0 ]]; then
         GREEN "$1 installed successfully!"
+        return 0
     else
         RED "Error installing $1."
-        exit 1
+        return 1
     fi
 }
 
@@ -66,30 +67,19 @@ if ! command -v sed &> /dev/null; then
     install_package sed
 fi
 
+if ! command -v apt-transport-https &> /dev/null; then
+    BLUE "Installing apt-transport-https..."
+    install_package apt-transport-https
+fi
+
 BLUE "Updating repositories..."
 apt update
 
 BLUE "Installing git..."
 install_package git
 
-BLUE "Installing Sublime Text..."
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-install_package apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-install_package sublime-text
-
 BLUE "Installing terminator..."
 install_package terminator
-
-BLUE "Setting terminator as the default terminal emulator..."
-TERMINAL_DESKTOP_PATH=$(find /usr/share/applications/ -name "xfce4-terminal.desktop")
-if [[ -f $TERMINAL_DESKTOP_PATH ]]; then
-    cp $TERMINAL_DESKTOP_PATH "$TERMINAL_DESKTOP_PATH.bak"  # Backup the original file
-    sed -i 's/Exec=xfce4-terminal/Exec=terminator/g' $TERMINAL_DESKTOP_PATH
-else
-    RED "xfce4-terminal.desktop not found. Skipping terminator default setting."
-fi
 
 BLUE "Installing pip..."
 install_package python-pip
@@ -103,14 +93,6 @@ install_package sqlitebrowser
 BLUE "Installing idle..."
 install_package idle
 
-BLUE "Downloading stegsolve.jar..."
-wget -q "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
-if [[ $? -ne 0 ]]; then
-    RED "Error downloading stegsolve.jar."
-    exit 1
-fi
-chmod +x "stegsolve.jar"
-
 BLUE "Installing fcrackzip..."
 install_package fcrackzip
 
@@ -123,11 +105,37 @@ install_package steghide
 BLUE "Installing ffmpeg..."
 install_package ffmpeg
 
-BLUE "Installing xrdp..."
 install_package xrdp 
-systemctl enable xrdp
-systemctl restart xrdp
-GREEN "To change default port, edit /etc/xrdp/xrdp.ini"
+if [[ $? -eq 0 ]]; then
+    systemctl enable xrdp
+    systemctl restart xrdp
+    GREEN "xrdp enabled successfully. To change default port, edit /etc/xrdp/xrdp.ini"
+else
+    RED "Skipping xrdp setup."
+fi
+
+BLUE "Installing Sublime Text..."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt update
+install_package sublime-text
+
+BLUE "Setting terminator as the default terminal emulator..."
+TERMINAL_DESKTOP_PATH=$(find /usr/share/applications/ -name "xfce4-terminal.desktop")
+if [[ -f $TERMINAL_DESKTOP_PATH ]]; then
+    cp $TERMINAL_DESKTOP_PATH "$TERMINAL_DESKTOP_PATH.bak"  # Backup the original file
+    sed -i 's/Exec=xfce4-terminal/Exec=terminator/g' $TERMINAL_DESKTOP_PATH
+else
+    RED "xfce4-terminal.desktop not found. Skipping terminator default setting."
+fi
+
+BLUE "Downloading stegsolve.jar..."
+wget -q "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
+if [[ $? -ne 0 ]]; then
+    RED "Error downloading stegsolve.jar."
+    exit 1
+fi
+chmod +x "stegsolve.jar"
 
 # Directory Changes
 read -p "${GREEN}Do you want to remove default directories and create new ones? (y/n): " response_dir
