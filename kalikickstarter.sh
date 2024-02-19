7d4e3e43cfa73e9556e73fe874dd0c6908cac2a7
Joshua Gilman <96031165+WhiteRqbbitt@users.noreply.github.com>
2024-02-19 14:09:13 -0500

    Update kalikickstarter.sh
    Bug fixes
    Readability changes
    Added more comments

--- kalikickstarter.sh
+++ kalikickstarter.sh
@@ -1,199 +1,154 @@
 #!/bin/bash
 
-# Define colors...
-RED=$(tput bold && tput setaf 1)
-GREEN=$(tput bold && tput setaf 2)
-YELLOW=$(tput bold && tput setaf 3)
-BLUE=$(tput bold && tput setaf 4)
-NC=$(tput sgr0)
+# Define colors
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+NC=$(tput sgr0) # No Color
 
-function RED() {
-    echo -e "\n${RED}$1${NC}"
+# Print messages in colors
+print_in_color() {
+    color=$1
+    text=$2
+    echo -e "\n${!color}${text}${NC}"
 }
 
-function GREEN() {
-    echo -e "\n${GREEN}$1${NC}"
-}
-
-function YELLOW() {
-    echo -e "\n${YELLOW}$1${NC}"
-}
-
-function BLUE() {
-    echo -e "\n${BLUE}$1${NC}"
-}
-
-function install_package() {
-    apt install -y "$1"
+# Function to install a package
+install_package() {
+    apt-get install -y "$1"
     if [[ $? -eq 0 ]]; then
-        GREEN "$1 installed successfully!"
-        return 0
+        print_in_color "GREEN" "$1 installed successfully!"
     else
-        RED "Error installing $1."
+        print_in_color "RED" "Error installing $1."
         return 1
     fi
 }
 
-# Testing if root...
-if [[ "$UID" -ne 0 ]]; then
-    RED "You must run this script as root!"
+# Check if running as root
+if [[ "$EUID" -ne 0 ]]; then
+    print_in_color "RED" "You must run this script as root!"
     exit 1
 fi
 
-echo -e "${GREEN}"
-cat << 'EOF'
- _   __      _ _ _   ___      _        _             _            
-| | / /     | (_) | / (_)    | |      | |           | |           
-| |/ /  __ _| |_| |/ / _  ___| | _____| |_ __ _ _ __| |_ ___ _ __ 
-|    \ / _` | | |    \| |/ __| |/ / __| __/ _` | '__| __/ _ \ '__|
-| |\  \ (_| | | | |\  \ | (__|   <\__ \ || (_| | |  | ||  __/ |   
-\_| \_/\__,_|_|_\_| \_/_|\___|_|\_\___/\__\__,_|_|   \__\___|_|   
-EOF
-echo -e "${NC}"
+# Display welcome message
+print_in_color "GREEN" "Welcome to the setup script!"
 
 # Ensure necessary tools are installed
-if ! command -v wget &> /dev/null; then
-    BLUE "Installing wget..."
-    install_package wget
-fi
+necessary_tools=("wget" "gpg" "sed" "apt-transport-https")
+for tool in "${necessary_tools[@]}"; do
+    if ! command -v $tool &> /dev/null; then
+        print_in_color "BLUE" "Installing $tool..."
+        install_package $tool
+    fi
+done
 
-if ! command -v gpg &> /dev/null; then
-    BLUE "Installing gpg..."
-    install_package gpg
-fi
+print_in_color "BLUE" "Updating repositories..."
+apt-get update
 
-if ! command -v sed &> /dev/null; then
-    BLUE "Installing sed..."
-    install_package sed
-fi
+# List of packages to install
+packages_to_install=("git" "terminator" "python-pip" "exiftool" "sqlitebrowser" "idle" "fcrackzip" "unrar" "steghide" "ffmpeg")
+for package in "${packages_to_install[@]}"; do
+    print_in_color "BLUE" "Installing $package..."
+    install_package $package
+done
 
-if ! command -v apt-transport-https &> /dev/null; then
-    BLUE "Installing apt-transport-https..."
-    install_package apt-transport-https
-fi
-
-BLUE "Updating repositories..."
-apt update
-
-BLUE "Installing git..."
-install_package git
-
-BLUE "Installing terminator..."
-install_package terminator
-
-BLUE "Installing pip..."
-install_package python-pip
-
-BLUE "Installing exiftool..."
-install_package exiftool
-
-BLUE "Installing sqlitebrowser..."
-install_package sqlitebrowser
-
-BLUE "Installing idle..."
-install_package idle
-
-BLUE "Installing fcrackzip..."
-install_package fcrackzip
-
-BLUE "Installing unrar..."
-install_package unrar
-
-BLUE "Installing steghide..."
-install_package steghide
-
-BLUE "Installing ffmpeg..."
-install_package ffmpeg
-
-BLUE "Installing IDA Free..."
-wget -q https://out7.hex-rays.com/files/idafree83_linux.run -O ~/Downloads/idafree.run
-chmod +x ~/Downloads/idafree.run
-~/Downloads/idafree.run
-echo 'export PATH=$PATH:/opt/idafree' >> ~/.zshrc
-
-BLUE "Installing xrdp..."
-install_package xrdp
+# Install IDA Free
+print_in_color "BLUE" "Installing IDA Free..."
+ida_url="https://out7.hex-rays.com/files/idafree83_linux.run"
+ida_dest="$HOME/Downloads/idafree.run"
+wget -q $ida_url -O $ida_dest && chmod +x $ida_dest && $ida_dest
 if [[ $? -eq 0 ]]; then
+    echo 'export PATH=$PATH:/opt/idafree' >> $HOME/.zshrc
+else
+    print_in_color "RED" "Error downloading or installing IDA Free."
+fi
+
+# Install xrdp
+print_in_color "BLUE" "Installing xrdp..."
+if install_package xrdp; then
     systemctl enable xrdp
     systemctl restart xrdp
-    GREEN "xrdp enabled successfully. To change default port, edit /etc/xrdp/xrdp.ini"
-else
-    RED "Skipping xrdp setup."
+    print_in_color "GREEN" "xrdp enabled successfully. To change default port, edit /etc/xrdp/xrdp.ini"
 fi
 
-BLUE "Installing Sublime Text..."
+# Install Sublime Text
+print_in_color "BLUE" "Installing Sublime Text..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-apt update
+apt-get update
 install_package sublime-text
 
-BLUE "Setting terminator as the default terminal emulator..."
+# Set terminator as default terminal emulator
+print_in_color "BLUE" "Setting terminator as the default terminal emulator..."
 TERMINAL_DESKTOP_PATH=$(find /usr/share/applications/ -name "xfce4-terminal.desktop")
 if [[ -f $TERMINAL_DESKTOP_PATH ]]; then
-    cp $TERMINAL_DESKTOP_PATH "$TERMINAL_DESKTOP_PATH.bak"  # Backup the original file
+    cp $TERMINAL_DESKTOP_PATH "${TERMINAL_DESKTOP_PATH}.bak"
     sed -i 's/Exec=xfce4-terminal/Exec=terminator/g' $TERMINAL_DESKTOP_PATH
 else
-    RED "xfce4-terminal.desktop not found. Skipping terminator default setting."
+    print_in_color "RED" "xfce4-terminal.desktop not found. Skipping terminator default setting."
 fi
 
-BLUE "Downloading stegsolve.jar..."
-wget -q "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
+# Download stegsolve.jar
+print_in_color "BLUE" "Downloading stegsolve.jar..."
+stegsolve_url="http://www.caesum.com/handbook/Stegsolve.jar"
+stegsolve_dest="stegsolve.jar"
+wget -q $stegsolve_url -O $stegsolve_dest && chmod +x $stegsolve_dest
 if [[ $? -ne 0 ]]; then
-    RED "Error downloading stegsolve.jar."
+    print_in_color "RED" "Error downloading stegsolve.jar."
     exit 1
 fi
-chmod +x "stegsolve.jar"
 
-# Directory Changes
-read -p "${GREEN}Do you want to remove default directories and create new ones? (y/n): " response_dir
+# Directory changes
+read -p "${GREEN}Do you want to remove default directories and create new ones? (y/n): ${NC}" response_dir
 if [[ "$response_dir" == "y" ]]; then
-    BLUE "Removing boilerplate home directories..."
+    print_in_color "BLUE" "Removing boilerplate home directories..."
     for dir in Music Pictures Public Templates Videos; do
-        rmdir ~/$dir
+        rmdir "$HOME/$dir"
     done
 
-    BLUE "Creating useful home directories..."
-    mkdir ~/CTF ~/Tools ~/Temp
+    print_in_color "BLUE" "Creating useful home directories..."
+    mkdir -p "$HOME/CTF" "$HOME/Tools" "$HOME/Temp"
 fi
 
-# Alias and environment variables
-ALIASES=(
-"alias nmap=\"grc nmap\""
-"alias please=\"sudo !!\""
-)
+# Alias and environment variables setup
+setup_aliases_and_env_vars() {
+    ALIASES=(
+    "alias nmap=\"grc nmap\""
+    "alias please=\"sudo !!\""
+    )
 
-ENV_VARS=(
-"export DIRS_LARGE=/usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt"
-"export DIRS_SMALL=/usr/share/seclists/Discovery/Web-Content/raft-small-directories.txt"
-"export FILES_LARGE=/usr/share/seclists/Discovery/Web-Content/raft-large-files.txt"
-"export FILES_SMALL=/usr/share/seclists/Discovery/Web-Content/raft-small-files.txt"
-"export BIG=/usr/share/seclists/Discovery/Web-Content/big.txt"
-"export ROCK=/usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt"
-)
+    ENV_VARS=(
+    "export DIRS_LARGE=/usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt"
+    "export DIRS_SMALL=/usr/share/seclists/Discovery/Web-Content/raft-small-directories.txt"
+    "export FILES_LARGE=/usr/share/seclists/Discovery/Web-Content/raft-large-files.txt"
+    "export FILES_SMALL=/usr/share/seclists/Discovery/Web-Content/raft-small-files.txt"
+    "export BIG=/usr/share/seclists/Discovery/Web-Content/big.txt"
+    "export ROCK=/usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt"
+    )
 
-# Ask user if they want to add the aliases and environment variables
-read -p "${GREEN}Do you want to add specified aliases and environment variables to ~/.zshrc? (y/n): " response
-
-if [[ "$response" == "y" ]]; then
     # Backup .zshrc
-    cp ~/.zshrc ~/.zshrc.bak
+    cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
 
-    # Append the alias
+    # Append the aliases
     for alias_entry in "${ALIASES[@]}"; do
-        grep -qF "$alias_entry" ~/.zshrc || echo "$alias_entry" >> ~/.zshrc
+        grep -qF "$alias_entry" "$HOME/.zshrc" || echo "$alias_entry" >> "$HOME/.zshrc"
     done
 
     # Append each environment variable
     for env_var in "${ENV_VARS[@]}"; do
-        grep -qF "$env_var" ~/.zshrc || echo "$env_var" >> ~/.zshrc
+        grep -qF "$env_var" "$HOME/.zshrc" || echo "$env_var" >> "$HOME/.zshrc"
     done
 
-    GREEN "Alias and environment variables have been appended to ~/.zshrc."
+    print_in_color "GREEN" "Alias and environment variables have been appended to ~/.zshrc."
+}
+
+read -p "${GREEN}Do you want to add specified aliases and environment variables to ~/.zshrc? (y/n): ${NC}" response
+if [[ "$response" == "y" ]]; then
+    setup_aliases_and_env_vars
 else
-    echo "Exiting without adding any aliases or environment variables."
+    print_in_color "YELLOW" "Exiting without adding any aliases or environment variables."
     exit 0
 fi
 
-source ~/.zshrc
-
-GREEN "Install complete!"
+print_in_color "GREEN" "Install complete! Please restart your terminal or source ~/.zshrc to apply changes."
